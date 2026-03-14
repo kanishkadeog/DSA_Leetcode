453d5183857171e553e0cb5c46bdae86d8f44dec
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-03-14 23:56:24 +0530

    Time: 7 ms (64.99%), Space: 56.8 MB (57.12%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+    const m = s.length;
+    const n = p.length;
+
+    const dp = Array.from({ length: m + 1 }, () =>
+        Array(n + 1).fill(false)
+    );
+
+    dp[0][0] = true;
+
+    // Handle patterns like a*, a*b*, etc.
+    for (let j = 2; j <= n; j++) {
+        if (p[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 2];
+        }
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+
+            // Case 1: direct match or '.'
+            if (p[j - 1] === s[i - 1] || p[j - 1] === '.') {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+
+            // Case 2: '*'
+            else if (p[j - 1] === '*') {
+                // Zero occurrence
+                dp[i][j] = dp[i][j - 2];
+
+                // One or more occurrence
+                if (
+                    p[j - 2] === s[i - 1] ||
+                    p[j - 2] === '.'
+                ) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j];
+                }
+            }
+        }
+    }
+
+    return dp[m][n];
+};
