3398445f2ad587c6276422ddcd00508dfbd072d4
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-03-15 13:30:13 +0530

    Time: 0 ms (100%), Space: 54.1 MB (36.94%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+
+    if (n === 0) return 1;
+
+    let N = n;
+    if (N < 0) {
+        x = 1 / x;
+        N = -N;
+    }
+
+    let result = 1;
+
+    while (N > 0) {
+        if (N % 2 === 1) {
+            result *= x;
+        }
+
+        x *= x;
+        N = Math.floor(N / 2);
+    }
+
+    return result;
+};
