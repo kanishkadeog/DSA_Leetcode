6ad80f8cc6bd970253b534d86d86e46b9dbace52
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-03-15 13:03:05 +0530

    Time: 1 ms (84.1%), Space: 53.7 MB (97.56%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    const n = nums.length;
+    if (n <= 1) return 0;
+
+    let jumps = 0;
+    let currentEnd = 0;
+    let farthest = 0;
+
+    for (let i = 0; i < n - 1; i++) {
+        farthest = Math.max(farthest, i + nums[i]);
+
+        // when we reach the end of current jump range
+        if (i === currentEnd) {
+            jumps++;
+            currentEnd = farthest;
+        }
+    }
+
+    return jumps;
+};
