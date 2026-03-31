7c4c65dac8b605393793fab25cd6a97041c6945d
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-03-31 09:53:15 +0530

    Time: 0 ms (100%), Space: 53.9 MB (81.48%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,35 @@
+var search = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return true;
+
+        // case: duplicates (can't decide)
+        if (nums[left] === nums[mid] && nums[mid] === nums[right]) {
+            left++;
+            right--;
+        }
+
+        // left half sorted
+        else if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        // right half sorted
+        else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return false;
+};
