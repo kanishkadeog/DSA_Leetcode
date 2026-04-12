c679e2a6271473b329766de0e3ed2cb95d17ec0e
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-04-12 08:44:47 +0530

    Time: 0 ms (100%), Space: 53.5 MB (79.22%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const res = [];
+
+    const dfs = (node) => {
+        if (!node) return;
+
+        dfs(node.left);     // Left
+        res.push(node.val); // Root
+        dfs(node.right);    // Right
+    };
+
+    dfs(root);
+    return res;
+};
