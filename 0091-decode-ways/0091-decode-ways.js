2943567d80256d4416be4711248ef05f10f7026a
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-04-12 08:33:11 +0530

    Time: 0 ms (100%), Space: 53.7 MB (81.92%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    const n = s.length;
+
+    // edge case
+    if (s[0] === '0') return 0;
+
+    const dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let one = s[i - 1];
+        let two = s.substring(i - 2, i);
+
+        // single digit
+        if (one !== '0') {
+            dp[i] += dp[i - 1];
+        }
+
+        // two digits
+        if (two >= "10" && two <= "26") {
+            dp[i] += dp[i - 2];
+        }
+    }
+
+    return dp[n];
+};
