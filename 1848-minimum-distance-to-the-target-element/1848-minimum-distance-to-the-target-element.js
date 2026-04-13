44b16fa6233566e9aeaec0d0412f24e27f8edece
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-04-13 17:46:55 +0530

    Time: 0 ms (100%), Space: 55 MB (6.12%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} start
+ * @return {number}
+ */
+var getMinDistance = function(nums, target, start) {
+    let minDist = Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            minDist = Math.min(minDist, Math.abs(i - start));
+        }
+    }
+
+    return minDist;
+};
