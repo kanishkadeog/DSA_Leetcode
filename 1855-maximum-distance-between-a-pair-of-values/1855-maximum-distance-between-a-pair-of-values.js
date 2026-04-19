aaf627f6630aa2d46307ca94e838f1700cac990a
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-04-19 09:13:10 +0530

    Time: 1 ms (96.67%), Space: 66.2 MB (96.67%) - LeetHub

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxDistance = function(nums1, nums2) {
+    let i = 0, j = 0;
+    let ans = 0;
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] <= nums2[j]) {
+            ans = Math.max(ans, j - i);
+            j++; // try to go farther
+        } else {
+            i++; // need smaller nums1[i]
+        }
+    }
+
+    return ans;
+};
