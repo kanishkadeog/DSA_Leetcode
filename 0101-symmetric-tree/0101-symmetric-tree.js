3b011536e060fb78c396f828045afa91634dca4f
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-04-22 11:21:58 +0530

    Time: 0 ms (100%), Space: 56.4 MB (73.17%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+
+    function isMirror(left, right) {
+        // both null
+        if (!left && !right) return true;
+
+        // one null
+        if (!left || !right) return false;
+
+        // values must match
+        if (left.val !== right.val) return false;
+
+        return (
+            isMirror(left.left, right.right) &&
+            isMirror(left.right, right.left)
+        );
+    }
+
+    return isMirror(root.left, root.right);
+};
