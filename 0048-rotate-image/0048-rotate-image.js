95f04c05153c1f1c4a42b091b03c3434f5fbcba9
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-04 17:43:27 +0530

    Time: 0 ms (100%), Space: 54.7 MB (27.81%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -2,12 +2,16 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+/**
+ * @param {number[][]} matrix
+ * @return {void}
+ */
 var rotate = function(matrix) {
     const n = matrix.length;
 
     // Step 1: Transpose
     for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
+        for (let j = i; j < n; j++) {
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
         }
     }
