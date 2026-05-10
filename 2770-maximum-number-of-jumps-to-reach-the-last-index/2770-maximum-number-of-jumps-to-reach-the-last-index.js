55798f0a627508bebf4f6bbd528ad53b6fe16378
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-10 09:41:09 +0530

    Time: 23 ms (38.89%), Space: 59.2 MB (88.89%) - LeetHub

---
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var maximumJumps = function(nums, target) {
+    let n = nums.length;
+
+    let dp = Array(n).fill(-1);
+    dp[0] = 0;
+
+    for (let i = 1; i < n; i++) {
+
+        for (let j = 0; j < i; j++) {
+
+            // valid jump
+            if (
+                dp[j] !== -1 &&
+                Math.abs(nums[i] - nums[j]) <= target
+            ) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    return dp[n - 1];
+};
