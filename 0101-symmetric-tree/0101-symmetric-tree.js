e8c10d4b8d65cebd0fccee4476c7a8918a24cd12
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-12 18:44:07 +0530

    Time: 0 ms (100%), Space: 56.3 MB (77.63%) - LeetHub - Time: O(?), Space: O(?)

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -30,3 +30,7 @@ var isSymmetric = function(root) {
 
     return isMirror(root.left, root.right);
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
