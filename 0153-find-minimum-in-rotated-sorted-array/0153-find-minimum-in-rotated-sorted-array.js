d397cd9395784afbe3e1d36f2466409ce3ee4e8c
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-14 10:07:39 +0530

    Time: 0 ms (100%), Space: 55.1 MB (7.01%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        // Minimum is in right half
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } 
+        // Minimum is in left half (including mid)
+        else {
+            right = mid;
+        }
+    }
+
+    return nums[left];
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
