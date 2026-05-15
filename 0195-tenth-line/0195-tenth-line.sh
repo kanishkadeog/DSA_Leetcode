4a72edd9228fd0034f2afabaf1fe4dac2a2ee88e
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-15 10:32:13 +0530

    Time: 23 ms (70.27%), Space: 3.8 MB (84.91%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0195-tenth-line/0195-tenth-line.sh
@@ -0,0 +1,8 @@
+# Read from the file file.txt and output the tenth line to stdout.
+
+# Solution 1: using sed
+sed -n '10p' file.txt
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
