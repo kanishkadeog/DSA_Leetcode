dfe01f1a032ac38328b9f790c4bab64cf36c9ef8
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-15 10:33:56 +0530

    Time: 26 ms (35.96%), Space: 3.9 MB (84.91%) - LeetHub - Time: O(?), Space: O(?)

--- 0195-tenth-line/0195-tenth-line.sh
+++ 0195-tenth-line/0195-tenth-line.sh
@@ -3,8 +3,11 @@
 # # Solution 1: using sed
 # sed -n '10p' file.txt
 
-# Solution 2: using awk
-awk 'NR==10' file.txt
+# # Solution 2: using awk
+# awk 'NR==10' file.txt
+
+# Solution 3: using tail + head
+tail -n +10 file.txt | head -n 1
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
