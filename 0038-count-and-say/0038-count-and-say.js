e20f4d01866d609676ef97fe70bf8718292fa1bb
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-15 08:54:05 +0530

    Time: 7 ms (25.63%), Space: 55.7 MB (63.54%) - LeetHub - Time: O(?), Space: O(?)

--- 0038-count-and-say/0038-count-and-say.js
+++ 0038-count-and-say/0038-count-and-say.js
@@ -6,20 +6,24 @@ var countAndSay = function(n) {
     let result = "1";
 
     for (let i = 2; i <= n; i++) {
-        let curr = "";
+        let temp = "";
         let count = 1;
 
         for (let j = 1; j <= result.length; j++) {
             if (result[j] === result[j - 1]) {
                 count++;
             } else {
-                curr += count + result[j - 1];
+                temp += count + result[j - 1];
                 count = 1;
             }
         }
 
-        result = curr;
+        result = temp;
     }
 
     return result;
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
