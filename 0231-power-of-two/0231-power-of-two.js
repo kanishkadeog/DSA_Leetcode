ab86330aa929b80e6aab7ba990d37f69294f8a18
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-17 09:19:25 +0530

    Time: 0 ms (100%), Space: 54.1 MB (99.54%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if (n <= 0) return false;
+    return (n & (n - 1)) === 0;
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
