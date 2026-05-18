68f0b3c6743ce6210e4d7af4c6825f95d4daabe2
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-18 10:34:03 +0530

    Time: 6 ms (93.29%), Space: 54 MB (98.09%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const count = new Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        count[s.charCodeAt(i) - 97]++;
+        count[t.charCodeAt(i) - 97]--;
+    }
+
+    for (let num of count) {
+        if (num !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
