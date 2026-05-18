aa437cf8d101a8e24170e10066ed4c993064d733
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-18 10:29:23 +0530

    Time: 57 ms (29.45%), Space: 58 MB (30.47%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+     node.val = node.next.val;
+    node.next = node.next.next;
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
