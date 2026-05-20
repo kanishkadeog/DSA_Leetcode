84c62fd762787143bafef65079bb5d7aa58d2ce7
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-20 09:25:36 +0530

    Time: 0 ms (100%), Space: 53.9 MB (20.08%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0292-nim-game/0292-nim-game.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+    return n % 4 !== 0;
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
