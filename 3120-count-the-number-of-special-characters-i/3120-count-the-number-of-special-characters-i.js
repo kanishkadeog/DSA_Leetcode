4ca7da85d530231214601bf98231590c448c6d41
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-05-26 08:23:10 +0530

    Time: 2 ms (52.38%), Space: 56 MB (69.84%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var numberOfSpecialChars = function(word) {
+
+    const lower = new Set();
+    const upper = new Set();
+
+    for (const ch of word) {
+
+        if (ch >= 'a' && ch <= 'z') {
+            lower.add(ch);
+        } else {
+            upper.add(ch.toLowerCase());
+        }
+    }
+
+    let count = 0;
+
+    for (const ch of lower) {
+        if (upper.has(ch)) {
+            count++;
+        }
+    }
+
+    return count;
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
