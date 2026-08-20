94e6ad4f04db38154a4b995df6573c1246cad9e4
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-08-20 14:29:22 +0530

    Time: 1 ms (71.88%), Space: 58 MB (21.88%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var resultArray = function(nums) {
+    const arr1 = [nums[0]];
+    const arr2 = [nums[1]];
+
+    for (let i = 2; i < nums.length; i++) {
+        if (arr1[arr1.length - 1] > arr2[arr2.length - 1]) {
+            arr1.push(nums[i]);
+        } else {
+            arr2.push(nums[i]);
+        }
+    }
+
+    return arr1.concat(arr2);
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
